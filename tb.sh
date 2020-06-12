870ebae90b8d15817d8e6af93abd3fca9fab91a4
Arcann <arcanneero@gmail.com>
2020-06-12 21:52:12 +0200

    update(GDT): consigne de déploiement

---
+++ tb.sh
@@ -0,0 +1,51 @@
+sed -i -e "s/:o2: - --- /| X /g" docs/*.md
+sed -i -e "s/:one:/| (1)/g" docs/*.md
+sed -i -e "s/:two:/| (2)/g" docs/*.md
+sed -i -e "s/:three:/| (3)/g" docs/*.md
+sed -i -e "s/:four:/| (4)/g" docs/*.md
+sed -i -e "s/:five:/| (5)/g" docs/*.md
+sed -i -e "s/:six:/| (6)/g" docs/*.md
+sed -i -e "s/:seven:/| (7)/g" docs/*.md
+sed -i -e "s/:eight:/| (8)/g" docs/*.md
+
+sed -i -e "s/Arcann/\n| Arcann | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/BobbyFett/\n| BobbyFett | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Caneasucre94/\n| Caneasucre94 | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/CaptainMoi/\n| CaptainMoi | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Cloud Sei/\n| Cloud Sei | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Dark R4gn4r/\n| Dark R4gn4r | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Dark Tenebrus/\n| Dark Tenebrus | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/DarkValos/\n| DarkValos | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Darnidec/\n| Darnidec | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Dave than/\n| Dave than | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Didjock Nelek/\n| Didjock Nelek | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Fard Ventel/\n| Fard Ventel | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Faroer Laike/\n| Faroer Laike | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/FontenayForce/\n| FontenayForce | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Jimi/\n| Jimi | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Joule/\n| Joule | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Kaleb the Hammer/\n| Kaleb the Hammer | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Lonrak/\n| Lonrak | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Madara/\n| Madara | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Maitre Labenere/\n| Maitre Labenere | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Mol Eliza/\n| Mol Eliza | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Mortagrim/\n| Mortagrim | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Movan Lipost/\n| Movan Lipost | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/MrElite95/\n| MrElite95 | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/NcgVader/\n| NcgVader | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Nevski/\n| Nevski | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Nzo/\n| Nzo | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/piicoo/\n| piicoo | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Psychoo/\n| Psychoo | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Psychøø85/\n| Psychøø85 | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Rhuynn/\n| Rhuynn | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Seigneur Lux Inda/\n| Seigneur Lux Inda | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/ShyFr3EzY/\n| ShyFr3EzY | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/styx/\n| styx | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Thraken Vih'Torr/\n| Thraken Vih'Torr | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Toine/\n| Toine | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Torin Woodle/\n| Torin Woodle | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/totor/\n| totor | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Tryphon/\n| Tryphon | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Veskasa Stargazer/\n| Veskasa Stargazer | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/yami/\n| yami | | | |\n|---|---|---|---|/g" docs/*.md
