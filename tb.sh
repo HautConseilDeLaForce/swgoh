fce20a50292700244e06fc435a34180a083447ec
Arcann <arcanneero@gmail.com>
2020-12-20 21:44:06 +0100

    update(GDT): consigne de déploiement

--- tb.sh
+++ tb.sh
@@ -19,9 +19,11 @@ sed -i -e "s/Dark Tenebrus/\n| Dark Tenebrus | | | |\n|---|---|---|---|/g" docs/
 sed -i -e "s/DarkValos/\n| DarkValos | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Darnidec/\n| Darnidec | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Dave than/\n| Dave than | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Dark kai/\n| Dark kai | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Dede Krawatt/\n| Dede Krawatt | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Didjock Nelek/\n| Didjock Nelek | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/ducan/\n| ducan | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Falen Lacer/\n| Falen Lacer | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Fard Ventel/\n| Fard Ventel | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Faroer Laike/\n| Faroer Laike | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/FontenayForce/\n| FontenayForce | | | |\n|---|---|---|---|/g" docs/*.md
@@ -29,7 +31,10 @@ sed -i -e "s/Jimi/\n| Jimi | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Joule/\n| Joule | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Kaleb the Hammer/\n| Kaleb the Hammer | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Lonrak/\n| Lonrak | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Luk la mol/\n| Luk la mol | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Madara/\n| Madara | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Maître Kéké one/\n| Maître Kéké one | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/Rhil Margos/\n| Rhil Margos | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Maitre Labenere/\n| Maitre Labenere | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Mol Eliza/\n| Mol Eliza | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Mortagrim/\n| Mortagrim | | | |\n|---|---|---|---|/g" docs/*.md
@@ -37,6 +42,7 @@ sed -i -e "s/Movan Lipost/\n| Movan Lipost | | | |\n|---|---|---|---|/g" docs/*.
 sed -i -e "s/MrElite95/\n| MrElite95 | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/NcgVader/\n| NcgVader | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Nevski/\n| Nevski | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/No0b/\n| No0b | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Nzo/\n| Nzo | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/piicoo/\n| piicoo | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Psychoo/\n| Psychoo | | | |\n|---|---|---|---|/g" docs/*.md
@@ -46,6 +52,7 @@ sed -i -e "s/Seigneur Lux Inda/\n| Seigneur Lux Inda | | | |\n|---|---|---|---|/
 sed -i -e "s/ShyFr3EzY/\n| ShyFr3EzY | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Shojiki/\n| Shojiki | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/styx/\n| styx | | | |\n|---|---|---|---|/g" docs/*.md
+sed -i -e "s/tessadar/\n| tessadar | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Thraken Vih'Torr/\n| Thraken Vih'Torr | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Toine/\n| Toine | | | |\n|---|---|---|---|/g" docs/*.md
 sed -i -e "s/Torin Woodle/\n| Torin Woodle | | | |\n|---|---|---|---|/g" docs/*.md
